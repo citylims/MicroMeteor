4ed8ca05a16679b2adee6e397c011e740da3955b
Ausin Formica <austin.formica@gmail.com>
2016-02-05 13:20:10 -0500

    hook up error reporting

--- client/templates/posts/post_submit.js
+++ client/templates/posts/post_submit.js
@@ -9,11 +9,11 @@ Template.postSubmit.events({
 
     Meteor.call('postInsert', post, function(err, res) {
       if (err) {
-        return alert(err.reason);
+        return throwError(err.reason);
       }
       //this is in lib/collections/posts.js
       if (res.postExists) {
-        alert('This link has already been posted');
+        throwError('This link has already been posted');
       }
 
       Router.go('postPage', {_id: result._id});
