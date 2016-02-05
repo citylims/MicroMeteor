a1a8361399d5720089fe7dfe9a6157637b289236
Ausin Formica <austin.formica@gmail.com>
2016-02-05 11:47:01 -0500

    rm insecure pkg, and permit post form

--- client/templates/posts/post_submit.js
+++ client/templates/posts/post_submit.js
@@ -6,8 +6,10 @@ Template.postSubmit.events({
       url: $(e.target).find('[name=url]').val(),
       title: $(e.target).find('[name=title]').val()
     };
-
+    
+    //set UID from mongo doc
     post._id = Posts.insert(post);
+    //pass post object to the router -- with the UID
     Router.go('postPage', post);
   }
 });
